46995fd60e535f998f062cf35dcc3f0776af9290
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-07-21 13:00:02 -0300

    ESM Export Types
    Learning about the four export types, export inline, export non-inline, export Default inline and export Default non-Inline

---
+++ TiposExportEmESM/exports.js
@@ -0,0 +1,17 @@
+function sum (a, b) {
+    return a+b
+}
+
+function mult (a, b)
+{
+    return a * b
+}
+
+function lessOne (a)
+{
+    return (a - 1)
+}
+
+export default lessOne
+
+export { sum, mult }
---
+++ TiposExportEmESM/script.js
@@ -0,0 +1,6 @@
+import { mult, sum } from "./exports.js"
+import lessOne from "./exports.js"
+
+console.log(sum(1, 5))
+console.log(mult (5, 2))
+console.log(lessOne(3))
